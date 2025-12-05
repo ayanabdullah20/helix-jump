b3bf6a1ee3650db385a7fe61960dac8dcdb6053a
Ayan Abdullah <137028034+ayanabdullah20@users.noreply.github.com>
2025-12-05 05:46:19 +0500

    3rd commit

--- Assets/Scripts/HelixController.cs
+++ Assets/Scripts/HelixController.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class HelixController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 100f; // Speed of rotation
+    public Vector2 startpos;
+    public bool isSwiping;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                startpos = touch.position;
+                isSwiping = true;
+            }
+            else if(touch.phase == TouchPhase.Moved && isSwiping)
+            {
+                RotateHelix(touch.position.x - startpos.x);
+                startpos = touch.position;
+            }
+            else if(touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                isSwiping = false;
+            }
+        }
+        else if(Input.GetMouseButtonDown(0))
+        {
+            startpos = Input.mousePosition;
+            isSwiping = true;
+        }
+        else if(Input.GetMouseButton(0) && isSwiping)
+        {
+            RotateHelix(Input.mousePosition.x - startpos.x);
+            startpos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isSwiping = false;
+        }
+    }
+    void RotateHelix(float swipedistance)
+    {
+        if(Mathf.Abs(swipedistance) > 20f)
+        {
+            float rotationAmount = (swipedistance > 0  ? -1f:1f )* rotationSpeed * Time.deltaTime;
+            transform.Rotate(0f, rotationAmount, 0f);
+        }
     }
 }
